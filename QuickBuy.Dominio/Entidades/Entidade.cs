da21d8af4e9695ac78f5a8461f5aa86e9c355406
Anderson Silva <andersonolisilva@gmail.com>
2022-07-03 13:09:15 -0300

    Validação nas classes de domínio.

--- QuickBuy.Dominio/Entidades/Entidade.cs
+++ QuickBuy.Dominio/Entidades/Entidade.cs
@@ -1,6 +1,29 @@
-﻿namespace QuickBuy.Dominio.Entidades
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace QuickBuy.Dominio.Entidades
 {
-    class Entidade
+    public abstract class Entidade
     {
+        public List<string> _mensagensValidacao { get; set; }
+        private List<string> MensagemValidacao { 
+            get { return _mensagensValidacao ?? (_mensagensValidacao = new List<string>()); }
+        }
+
+        protected void LimparMensangesValidacao()
+        {
+            MensagemValidacao.Clear();
+        }
+
+        protected void AdicionarCritica(string mensagem)
+        {
+            MensagemValidacao.Add(mensagem);
+        }
+
+        public abstract void Validade();
+        protected bool EhValido
+        {
+            get { return !MensagemValidacao.Any(); }
+        }
     }
 }
--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,10 +1,11 @@
 ﻿using QuickBuy.Dominio.ObjetoDeValor;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Pedido
+    public class Pedido : Entidade
     {
         public int Id { get; set; }
 
@@ -19,12 +20,24 @@ namespace QuickBuy.Dominio.Entidades
         public int NumeroEndereco { get; set; }
         public int FormaPagamentoId { get; set; }
         public FormaPagamento FormaPagamento { get; set; }
-
-
         /// <summary>
         /// Pedido deve ter pelo menos um pedido
         /// ou muitos itens de pedidos
         /// </summary>
         public ICollection<ItemPedido> ItensPedido { get; set; }
+
+        public override void Validade()
+        {
+            LimparMensangesValidacao();
+
+            if (!ItensPedido.Any())
+                AdicionarCritica("Crítica - Pedido não pode ficar sem item de pedido.");
+
+            if (string.IsNullOrEmpty(CEP))
+                AdicionarCritica("Crítica - CEP deve estar preenchido");
+
+            if (FormaPagamentoId == 0)
+                AdicionarCritica("Crítica - Não foi informado a forma de pagamento.");
+        }
     }
 }
